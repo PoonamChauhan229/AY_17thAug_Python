65d4003ff8ae55462ac11a1a838f5e3acf410891
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2025-08-17 13:05:43 +0530

    first commit

---
+++ Introduction/firstProgram.py
@@ -0,0 +1,16 @@
+# Commenting 
+# ShortCut Key for Commenting >> cmd /
+
+# Write 1st program - Hello World
+# print()
+# Method and Property
+# end with () >>Method
+
+# print()  >>Put iniside print as it is 
+print("Hello World")
+
+#cd >>Change directory
+
+# Terminal  >> urin correct path- directory
+# py filename 
+
